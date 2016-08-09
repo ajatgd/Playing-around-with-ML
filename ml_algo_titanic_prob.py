37e55701d458bea16c2156361153e4f1c087c5df
ajatgd <akshayvashistha1995@gmail.com>
2016-08-09 23:56:48 +0530

    first commit

---
+++ ml_algo_titanic_prob.py
@@ -0,0 +1,208 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 09 22:46:44 2016
+
+@author: ajatgd
+"""
+# application of different ml algo on titanic dataset.
+
+import numpy as np
+import pandas
+import re
+import operator
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.cross_validation import KFold
+from sklearn import cross_validation
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+
+#read given csv
+titanic = pandas.read_csv("titanic_train.csv")
+
+# Print rows of the dataframe
+print(titanic.describe())
+# accessing titanic dataframe
+titanic.Age=titanic.Age.fillna(titanic.Age.median())
+print(titanic["Sex"].unique())
+#assign 0 to male and 1 to females
+titanic.loc[titanic["Sex"] == "male", "Sex"] = 0
+titanic.loc[titanic['Sex'] == 'female','Sex']=1
+# assigning unique values to embarked
+print(titanic["Embarked"].unique())
+titanic.Embarked=titanic['Embarked'].fillna('S')
+titanic.loc[titanic['Embarked'] == 'S', 'Embarked']=0
+titanic.loc[titanic['Embarked'] == 'C', 'Embarked']=1
+titanic.loc[titanic['Embarked'] == 'Q', 'Embarked']=2
+
+predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
+
+# Initialize our algorithm class
+alg = LinearRegression()
+# Generate cross validation folds for the titanic dataset that return the row indices 
+kf = KFold(titanic.shape[0], n_folds=3, random_state=1)
+
+predictions = []
+for train, test in kf:
+    # The predictors we're using the train the algorithm.  Note how we only take the rows in the train folds.
+    train_predictors = (titanic[predictors].iloc[train,:])
+    # The target we're using to train the algorithm.
+    train_target = titanic["Survived"].iloc[train]
+    # Training the algorithm using the predictors and target.
+    alg.fit(train_predictors, train_target)
+    # We can now make predictions on the test fold
+    test_predictions = alg.predict(titanic[predictors].iloc[test,:])
+    predictions.append(test_predictions)
+    
+# The predictions are in three separate numpy arrays.  Concatenate them into one.  
+# We concatenate them on axis 0, as they only have one axis.
+predictions = np.concatenate(predictions, axis=0)
+
+# Map predictions to outcomes (only possible outcomes are 1 and 0)
+predictions[predictions > .5] = 1
+predictions[predictions <=.5] = 0
+accuracy= sum(predictions[predictions == titanic.Survived]) /len(predictions)
+
+# using logistic regression for improved results
+
+# Initialize our algorithm
+alg = LogisticRegression(random_state=1)
+# Compute the accuracy score for all the cross validation folds.  (much simpler than what we did before!)
+scores = cross_validation.cross_val_score(alg, titanic[predictors], titanic["Survived"], cv=3)
+# Take the mean of the scores (because we have one for each fold)
+print(scores.mean())
+
+alg = RandomForestClassifier(random_state=1, n_estimators=10, min_samples_split=2, min_samples_leaf=1)
+scores=cross_validation.cross_val_score(alg,titanic[predictors],titanic['Survived'],cv=3)
+print(scores.mean())
+
+#after changing parameters
+alg = RandomForestClassifier(random_state=1, n_estimators=150, min_samples_split=4, min_samples_leaf=2)
+scores=cross_validation.cross_val_score(alg,titanic[predictors],titanic['Survived'],cv=3)
+print(scores.mean())
+# Generating a familysize column
+titanic["FamilySize"] = titanic["SibSp"] + titanic["Parch"]
+
+# The .apply method generates a new series
+titanic["NameLength"] = titanic["Name"].apply(lambda x: len(x))
+# A function to get the title from a name.
+def get_title(name):
+    # Use a regular expression to search for a title.  Titles always consist of capital and lowercase letters, and end with a period.
+    title_search = re.search(' ([A-Za-z]+)\.', name)
+    # If the title exists, extract and return it.
+    if title_search:
+        return title_search.group(1)
+    return ""
+
+# Get all the titles and print how often each one occurs.
+titles = titanic["Name"].apply(get_title)
+print(pandas.value_counts(titles))
+
+# Map each title to an integer.  Some titles are very rare, and are compressed into the same codes as other titles.
+title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Dr": 5, "Rev": 6, "Major": 7, "Col": 7, "Mlle": 8, "Mme": 8, "Don": 9, "Lady": 10, "Countess": 10, "Jonkheer": 10, "Sir": 9, "Capt": 7, "Ms": 2}
+for k,v in title_mapping.items():
+    titles[titles == k] = v
+
+# Verify that we converted everything.
+print(pandas.value_counts(titles))
+
+# Add in the title column.
+titanic["Title"] = titles
+# A dictionary mapping family name to id
+family_id_mapping = {}
+
+# A function to get the id given a row
+def get_family_id(row):
+    # Find the last name by splitting on a comma
+    last_name = row["Name"].split(",")[0]
+    # Create the family id
+    family_id = "{0}{1}".format(last_name, row["FamilySize"])
+    # Look up the id in the mapping
+    if family_id not in family_id_mapping:
+        if len(family_id_mapping) == 0:
+            current_id = 1
+        else:
+            # Get the maximum id from the mapping and add one to it if we don't have an id
+            current_id = (max(family_id_mapping.items(), key=operator.itemgetter(1))[1] + 1)
+        family_id_mapping[family_id] = current_id
+    return family_id_mapping[family_id]
+
+# Get the family ids with the apply method
+family_ids = titanic.apply(get_family_id, axis=1)
+
+# There are a lot of family ids, so we'll compress all of the families under 3 members into one code.
+family_ids[titanic["FamilySize"] < 3] = -1
+
+# Print the count of each unique id.
+print(pandas.value_counts(family_ids))
+
+titanic["FamilyId"] = family_ids
+
+
+
+
+#modifying algo
+predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked", "FamilySize", "Title", "FamilyId"]
+
+# Perform feature selection
+selector = SelectKBest(f_classif, k=5)
+selector.fit(titanic[predictors], titanic["Survived"])
+
+# Get the raw p-values for each feature, and transform from p-values into scores
+scores = -np.log10(selector.pvalues_)
+
+# Plot the scores.  See how "Pclass", "Sex", "Title", and "Fare" are the best?
+plt.bar(range(len(predictors)), scores)
+plt.xticks(range(len(predictors)), predictors, rotation='vertical')
+plt.show()
+
+# Pick only the four best features.
+predictors = ["Pclass", "Sex", "Fare", "Title"]
+
+alg = RandomForestClassifier(random_state=1, n_estimators=150, min_samples_split=8, min_samples_leaf=4)
+scores=cross_validation.cross_val_score(alg,titanic[predictors],titanic['Survived'],cv=3)
+print(scores.mean())
+
+
+# The algorithms we want to ensemble.
+# We're using the more linear predictors for the logistic regression, and everything with the gradient boosting classifier.
+algorithms = [
+    [GradientBoostingClassifier(random_state=1, n_estimators=25, max_depth=3), ["Pclass", "Sex", "Age", "Fare", "Embarked", "FamilySize", "Title", "FamilyId"]],
+    [LogisticRegression(random_state=1), ["Pclass", "Sex", "Fare", "FamilySize", "Title", "Age", "Embarked"]]
+]
+
+# Initialize the cross validation folds
+kf = KFold(titanic.shape[0], n_folds=3, random_state=1)
+
+predictions = []
+for train, test in kf:
+    train_target = titanic["Survived"].iloc[train]
+    full_test_predictions = []
+    # Make predictions for each algorithm on each fold
+    for alg, predictors in algorithms:
+        # Fit the algorithm on the training data.
+        alg.fit(titanic[predictors].iloc[train,:], train_target)
+        # Select and predict on the test fold.  
+        # The .astype(float) is necessary to convert the dataframe to all floats and avoid an sklearn error.
+        test_predictions = alg.predict_proba(titanic[predictors].iloc[test,:].astype(float))[:,1]
+        full_test_predictions.append(test_predictions)
+    # Use a simple ensembling scheme -- just average the predictions to get the final classification.
+    test_predictions = (full_test_predictions[0] + full_test_predictions[1]) / 2
+    # Any value over .5 is assumed to be a 1 prediction, and below .5 is a 0 prediction.
+    test_predictions[test_predictions <= .5] = 0
+    test_predictions[test_predictions > .5] = 1
+    predictions.append(test_predictions)
+
+# Put all the predictions together into one array.
+predictions = np.concatenate(predictions, axis=0)
+
+# Compute accuracy by comparing to the training data.
+accuracy = sum(predictions[predictions == titanic["Survived"]]) / len(predictions)
+print(accuracy)
+
+
+
+    
+
